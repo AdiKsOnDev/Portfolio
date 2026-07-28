bf735c93b33733cbdb6986c5758db45d6ad5f7ad
AdiKsOnDev <adilalizada@pm.me>
2026-07-28 15:35:15 +0400

    chore(data): remove skills demo data

--- lib/data/index.ts
+++ lib/data/index.ts
@@ -1,5 +1,4 @@
 export * from "./profile";
-export * from "./skills";
 export * from "./experience";
 export * from "./projects";
 export * from "./blogs";
--- lib/data/skills.ts
+++
@@ -1,6 +0,0 @@
-import skillsData from "@/data/content/skills.json";
-import type { SkillsData } from "@/types";
-
-export function getSkills(): SkillsData {
-  return skillsData as SkillsData;
-}
